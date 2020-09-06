29c14ad94408e413041d6e56ab872ded0af78c4d
Paweł Renc <rencpawe@gmail.com>
2020-09-07 01:14:20 +0200

    Implement preliminarly nrs constraint

--- src/score.jl
+++ src/score.jl
@@ -6,63 +6,104 @@ using Statistics
 using ..NurseSchedules
 
 
-function score(schedule_shifts::ScheduleShifts, month_info::Dict{String,Any}, workers_info::Dict{String,Any})::Int
+function score(
+    schedule_shifts::ScheduleShifts,
+    month_info::Dict{String,Any},
+    workers_info::Dict{String,Any},
+)::Int
     workers, shifts = schedule_shifts
     penalty = 0
 
-    penalty += check_workers_presence(shifts, month_info)
+    penalty += ck_workers_presence(schedule_shifts, month_info, workers_info)
 
-    penalty += check_workers_rights(workers, shifts)
 
-    penalty += check_workers_worktime(workers, shifts, workers_info)
+    penalty += ck_workers_rights(workers, shifts)
+
+    penalty += ck_workers_worktime(workers, shifts, workers_info)
 
     return penalty
 end
 
-function check_workers_presence(shifts, month_info)::Int
+function ck_workers_presence(
+    schedule_shifts::ScheduleShifts,
+    month_info::Dict{String,Any},
+    workers_info::Dict{String,Any},
+)::Int
+    workers, shifts = schedule_shifts
     penalty = 0
-    for day in axes(shifts, 2)
-        req_nrs_day::Int = ceil(month_info["children_number"][day] / REQ_CHLDN_PER_NRS_DAY)
-        req_nrs_night::Int =
-            ceil(month_info["children_number"][day] / REQ_CHLDN_PER_NRS_NIGHT)
-
-        act_nrs_night = count(s -> (s in SHIFTS_NIGHT), shifts[:, day])
-
-        act_nrs_day = count(s -> (s in SHIFTS_FULL_DAY), shifts[:, day])
-        act_nrs_day += min(
-            count(s -> (s == R), shifts[:, day]),
-            count(s -> (s in [P, PN]), shifts[:, day]),
-        )
-        # night shifts complement day shifts
-        act_nrs_day = (act_nrs_day > act_nrs_night) ? act_nrs_night : act_nrs_day
-
-        missing_nrs_day = req_nrs_day - act_nrs_day
-        missing_nrs_day = (missing_nrs_day < 0) ? 0 : missing_nrs_day
-        missing_nrs_night = req_nrs_night - act_nrs_night
-        missing_nrs_night = (missing_nrs_night < 0) ? 0 : missing_nrs_night
-
-        # penalty is charged only for nurses lacking during the day
-        day_pen = missing_nrs_day * PEN_LACKING_NURSE
-        penalty += day_pen
-
-        if day_pen > 0
-            error_details = ""
-            if missing_nrs_day > 0
-                error_details *= "\nExpected '$(req_nrs_day)', got '$(act_nrs_day)' in the day."
-            end
-            if missing_nrs_night > 0
-                error_details *= "\nExpected '$(req_nrs_night)', got '$(act_nrs_night)' at night."
-            end
-            @debug "Lacking nurses on day '$day'." * error_details
-        end
+    for day_no in axes(shifts, 2)
+        day_shifts = shifts[:, day_no]
+        penalty += ck_workers_to_children(day_no, day_shifts, month_info)
+        penalty += ck_nurse_presence(day_no, workers, day_shifts, workers_info)
     end
     if penalty > 0
-        @debug "Lacking nurses total penalty: $(penalty)"
+        @debug "Lacking workers total penalty: $(penalty)"
     end
     return penalty
 end
 
-function check_workers_rights(workers, shifts)::Int
+function ck_workers_to_children(
+    day::Int,
+    day_shifts::Array{String,1},
+    month_info::Dict{String,Any},
+)::Int
+    penalty = 0
+    req_nrs_day::Int = ceil(month_info["children_number"][day] / REQ_CHLDN_PER_NRS_DAY)
+    req_nrs_night::Int = ceil(month_info["children_number"][day] / REQ_CHLDN_PER_NRS_NIGHT)
+
+    act_nrs_night = count(s -> (s in SHIFTS_NIGHT), day_shifts)
+
+    act_nrs_day = count(s -> (s in SHIFTS_FULL_DAY), day_shifts)
+    act_nrs_day +=
+        min(count(s -> (s == R), day_shifts), count(s -> (s in [P, PN]), day_shifts))
+    # night shifts complement day shifts
+    act_nrs_day = min(act_nrs_day, act_nrs_night)
+
+    missing_nrs_day = req_nrs_day - act_nrs_day
+    missing_nrs_day = (missing_nrs_day < 0) ? 0 : missing_nrs_day
+    missing_nrs_night = req_nrs_night - act_nrs_night
+    missing_nrs_night = (missing_nrs_night < 0) ? 0 : missing_nrs_night
+
+    # penalty is charged only for nurses lacking during the day
+    day_pen = missing_nrs_day * PEN_LACKING_WORKER
+    penalty += day_pen
+
+    if day_pen > 0
+        error_details = ""
+        if missing_nrs_day > 0
+            error_details *= "\nExpected '$(req_nrs_day)', got '$(act_nrs_day)' in the day."
+        end
+        if missing_nrs_night > 0
+            error_details *= "\nExpected '$(req_nrs_night)', got '$(act_nrs_night)' at night."
+        end
+        @debug "Lacking nurses on day '$day'." * error_details
+    end
+    return penalty
+end
+
+function ck_nurse_presence(day::Int, wrks, day_shifts, workers_info)
+    penalty = 0
+    nrs_shifts = [
+        shift
+        for
+        (wrk, shift) in zip(wrks, day_shifts) if workers_info["type"][wrk] == "NURSE"
+    ]
+    if isempty(SHIFTS_MORNING ∩ nrs_shifts)
+        @debug "Lacking a nurse in the morning on day '$day'"
+        penalty += PEN_LACKING_NURSE
+    end
+    if isempty(SHIFTS_AFTERNOON ∩ nrs_shifts)
+        @debug "Lacking a nurse in the afternoon on day '$day'"
+        penalty += PEN_LACKING_NURSE
+    end
+    if isempty(SHIFTS_NIGHT ∩ nrs_shifts)
+        @debug "Lacking a nurse in the night on day '$day'"
+        penalty += PEN_LACKING_NURSE
+    end
+    return penalty
+end
+
+function ck_workers_rights(workers, shifts)::Int
     penalty = 0
     for worker_no in axes(shifts, 1)
         long_breaks = fill(false, ceil(Int, size(shifts, 2) / length(DAYS_OF_WEEK)))
@@ -74,7 +115,8 @@ function check_workers_rights(workers, shifts)::Int
             end
 
             if shifts[worker_no, shift_no] in keys(DISALLOWED_SHIFTS_SEQS) &&
-               shifts[worker_no, shift_no + 1] in DISALLOWED_SHIFTS_SEQS[shifts[worker_no, shift_no]]
+               shifts[worker_no, shift_no+1] in
+               DISALLOWED_SHIFTS_SEQS[shifts[worker_no, shift_no]]
 
                 penalty += PEN_DISALLOWED_SHIFT_SEQ
                 @debug "Worker '$(workers[worker_no])' has a disallowed shift sequence " *
@@ -84,7 +126,7 @@ function check_workers_rights(workers, shifts)::Int
 
             if shift_no % length(DAYS_OF_WEEK) != 0 && # long break between weeks does not count
                shifts[worker_no, shift_no] in LONG_BREAK_SEQ[1] &&
-               shifts[worker_no, shift_no + 1] in LONG_BREAK_SEQ[2]
+               shifts[worker_no, shift_no+1] in LONG_BREAK_SEQ[2]
 
                 long_breaks[Int(ceil(shift_no / length(DAYS_OF_WEEK)))] = true
             end
@@ -102,7 +144,7 @@ function check_workers_rights(workers, shifts)::Int
     return penalty
 end
 
-function check_workers_worktime(workers, shifts, workers_info)::Int
+function ck_workers_worktime(workers, shifts, workers_info)::Int
     penalty = 0
     workers_worktime = Dict()
     weeks_num = ceil(Int, size(shifts, 2) / length(DAYS_OF_WEEK))
