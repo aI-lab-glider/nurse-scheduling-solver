a8feff450ade145f2a8a4e5c03e395e3471df5ce
Paweł Renc <rencpawe@gmail.com>
2020-09-16 00:02:58 +0200

    Fix tabu search and schedule data

--- examples/basic_tabu_search.jl
+++ examples/basic_tabu_search.jl
@@ -12,8 +12,7 @@ BestResult = @NamedTuple{shifts::Shifts, score::Number}
 
 ITERATION_NUMBER = 200
 INITIAL_MAX_TABU_SIZE = 100
-TABU_SIZE_INCREASE = 20
-SCHEDULE_PATH = "schedules/schedule_2016_august_unsolvable.json"
+SCHEDULE_PATH = "schedules/schedule_2016_september.json"
 
 function in(shifts::Shifts, tabu_list::Vector{BestResult})
     findfirst(record -> record.shifts == shifts, tabu_list) != nothing
@@ -34,7 +33,7 @@ tabu_list = Vector{BestResult}()
 push!(tabu_list, best_res)
 max_tabu_size = INITIAL_MAX_TABU_SIZE
 
-for i in 1:ITERATION_NUMBER
+for i = 1:ITERATION_NUMBER
     result_improved = false
 
     global best_iter_res = BestResult((shifts = best_iter_res.shifts, score = Inf))
@@ -45,25 +44,31 @@ for i in 1:ITERATION_NUMBER
         if best_iter_res.score > candidate_score && !(candidate_shifts in tabu_list)
             global best_iter_res = BestResult((candidate_shifts, candidate_score))
         end
-        if best_res.score > best_iter_res.score
-            println("Penalty reduced in iter '$(i)': '$(best_res.score)' -> '$(best_iter_res.score)'")
-            global best_res = best_iter_res
-            result_improved = true
-        end
-        push!(tabu_list, best_iter_res)
-        while length(tabu_list) > max_tabu_size
-            popfirst!(tabu_list)
-        end
     end
+
+    println("[Iteration '$(i)']")
+    if best_res.score > best_iter_res.score
+        println("Penalty: '$(best_res.score)' -> '$(best_iter_res.score)' ($(best_iter_res.score - best_res.score))")
+        global best_res = best_iter_res
+        result_improved = true
+    end
+
+    push!(tabu_list, best_iter_res)
+
+    while length(tabu_list) > max_tabu_size
+        popfirst!(tabu_list)
+    end
+
+    println("Tabu unique length = ", length(unique([z.shifts for z in tabu_list])))
 
     if result_improved
         global max_tabu_size = INITIAL_MAX_TABU_SIZE
-    else
-        global max_tabu_size += TABU_SIZE_INCREASE
-        println("Increasing max tabu size to: $(max_tabu_size)")
+    elseif length(tabu_list) == max_tabu_size
+        global max_tabu_size += 1
+        println("Incrementing max tabu size to: $(max_tabu_size)")
     end
 
-    if best_res.score == 0 || length(tabu_list) > length(nbhd)
+    if best_res.score == 0
         println("We will not be better, finishing.")
         break
     end
@@ -71,6 +76,7 @@ for i in 1:ITERATION_NUMBER
 end
 
 with_logger(logger) do
-    improved_penalty, errors = score((workers, best_res.shifts), month_info, workers_info, true)
+    improved_penalty, errors =
+        score((workers, best_res.shifts), month_info, workers_info, true)
     println("Penaly improved: '$(penalty)' -> '$(improved_penalty)'")
 end
