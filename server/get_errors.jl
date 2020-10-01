0ff6d8f5ba96d8b825c21159e8ca7e68314370d8
Paweł Renc <rencpawe@gmail.com>
2020-10-01 21:28:45 +0200

    Update server with the newest algorithm

--- server/get_errors.jl
+++ server/get_errors.jl
@@ -1,18 +1,11 @@
-import Base.in
-
-BestResult = @NamedTuple{shifts::Shifts, score::Number}
-
 function get_errors(schedule_data)
-    function in(shifts::Shifts, tabu_list::Vector{BestResult})
-        findfirst(record -> record.shifts == shifts, tabu_list) != nothing
-    end
 
-    global nurse_schedule = Schedule(schedule_data)
+    nurse_schedule = Schedule(schedule_data)
 
-    global schedule_shifts = get_shifts(nurse_schedule)
-    global month_info = get_month_info(nurse_schedule)
-    global workers_info = get_workers_info(nurse_schedule)
+    schedule_shifts = get_shifts(nurse_schedule)
+    month_info = get_month_info(nurse_schedule)
+    workers_info = get_workers_info(nurse_schedule)
 
-    global _, errors = score(schedule_shifts, month_info, workers_info, true)
-    errors
+    _, errors = score(schedule_shifts, month_info, workers_info, true)
+    return errors
 end
--- server/repair_schedule.jl
+++ server/repair_schedule.jl
@@ -1,36 +1,103 @@
+include("../src/NursesScheduling.jl")
+include("parameters.jl")
+using .NurseSchedules
+using .NurseSchedules: Shifts
+using Logging
+using StatsBase: sample
+using DataStructures: OrderedDict
+
 import Base.in
+
+logger = ConsoleLogger(stderr, Logging.Debug)
 
 BestResult = @NamedTuple{shifts::Shifts, score::Number}
 
-function repair_schedule(schedule_data)
-    ITERATION_NUMBER = 20
-    INITIAL_MAX_TABU_SIZE = 20
-    INC_TABU_SIZE_ITER = 5
+function in(shifts::Shifts, tabu_list::OrderedDict{UInt,BestResult})
+    haskey(tabu_list, hash(shifts))
+end
 
-    function in(shifts::Shifts, tabu_list::Vector{BestResult})
-        findfirst(record -> record.shifts == shifts, tabu_list) != nothing
+function popfirst!(dict::OrderedDict)
+    (key, _), _ = iterate(dict)
+    delete!(dict, key)
+end
+
+function eval_frozen_shifts(
+    month_info,
+    errors::Vector,
+    no_improved_iters::Int,
+    workers,
+)::Vector{Tuple{Int,Int}}
+    num_days = length(month_info["children_number"])
+    num_wrks = length(workers)
+
+    always_frozen_shifts = [
+        wrk === 0 ? (wrk, day_no) : (findfirst(w -> w === wrk, workers), day_no)
+        for (wrk, day_no) in month_info["frozen_shifts"]
+    ]
+
+    exclusion_range = if no_improved_iters > FULL_NBHD_ITERS
+        return always_frozen_shifts
+    elseif no_improved_iters > EXTENDED_NBHD_ITERS
+        1
+    else
+        0
     end
 
-    global nurse_schedule = Schedule(schedule_data)
+    day_errors = filter(error -> haskey(error, "day"), errors)
+    iter_frozen_shifts = if !isempty(day_errors)
+        changeable_days = Vector{Int}()
+        for error in day_errors
+            for i = 0:exclusion_range
+                push!(changeable_days, error["day"] + i)
+                i > 0 && push!(changeable_days, error["day"] - i)
+            end
+        end
 
-    global schedule_shifts = get_shifts(nurse_schedule)
-    global workers, shifts = schedule_shifts
-    global month_info = get_month_info(nurse_schedule)
-    global workers_info = get_workers_info(nurse_schedule)
+        println("Days being improved: $(length(Set(changeable_days)))")
+        [(0, day_no) for day_no in setdiff(Set(1:num_days), Set(changeable_days))]
+    else
+        worker_errors = filter(error -> !haskey(error, "day"), errors)
 
-    global penalty = score(schedule_shifts, month_info, workers_info)
-    global best_res = BestResult((shifts = shifts, score = penalty))
-    global best_iter_res = BestResult((shifts = best_res.shifts, score = Inf))
+        changeable_wrks = [
+            findfirst(wrk_id -> wrk_id == error["worker"], workers)
+            for error in worker_errors
+        ]
+        no_improved_iters == 1 && append!(
+            changeable_wrks,
+            sample(1:num_wrks, floor(Int, num_wrks * WRKS_RANDOM_FACTOR), replace = false),
+        )
+        println("Workers being improved: $(length(Set(changeable_wrks)))")
+        [(wrk_no, 0) for wrk_no in setdiff(Set(1:num_wrks), Set(changeable_wrks))]
+    end
 
-    global tabu_list = Vector{BestResult}()
-    push!(tabu_list, best_res)
-    global max_tabu_size = INITIAL_MAX_TABU_SIZE
-    global no_improved_iters = 0
+    vcat(always_frozen_shifts, iter_frozen_shifts)
+end
+
+function repair_schedule(schedule_data)
+
+    nurse_schedule = Schedule(schedule_data)
+
+    schedule_shifts = get_shifts(nurse_schedule)
+    workers, shifts = schedule_shifts
+    month_info = get_month_info(nurse_schedule)
+    workers_info = get_workers_info(nurse_schedule)
+
+    penalty = score(schedule_shifts, month_info, workers_info)
+    best_res = BestResult((shifts = shifts, score = penalty))
+    best_iter_res = BestResult((shifts = best_res.shifts, score = Inf))
+
+    tabu_list = OrderedDict{UInt,BestResult}()
+    tabu_list[hash(best_res.shifts)] = best_res
+    max_tabu_size = INITIAL_MAX_TABU_SIZE
+    no_improved_iters = 0
 
     for i = 1:ITERATION_NUMBER
-        best_iter_res = BestResult((shifts = best_iter_res.shifts, score = Inf))
+        global best_iter_res = BestResult((shifts = best_iter_res.shifts, score = Inf))
 
-        nbhd = Neighborhood(best_iter_res.shifts)
+        _, errors = score((workers, best_iter_res.shifts), month_info, workers_info, true)
+        println("[Iteration '$(i)']")
+        act_frozen_days = eval_frozen_shifts(month_info, errors, no_improved_iters, workers)
+        nbhd = Neighborhood(best_iter_res.shifts, act_frozen_days)
         for candidate_shifts in nbhd
             candidate_score = score((workers, candidate_shifts), month_info, workers_info)
             if best_iter_res.score > candidate_score && !(candidate_shifts in tabu_list)
@@ -39,13 +106,14 @@ function repair_schedule(schedule_data)
         end
 
         if best_res.score > best_iter_res.score
+            println("Penalty: '$(best_res.score)' -> '$(best_iter_res.score)' ($(best_iter_res.score - best_res.score))")
             global best_res = best_iter_res
             global no_improved_iters = 0
         else
             global no_improved_iters += 1
         end
 
-        push!(tabu_list, best_iter_res)
+        tabu_list[hash(best_iter_res.shifts)] = best_iter_res
 
         while length(tabu_list) > max_tabu_size
             popfirst!(tabu_list)
@@ -53,14 +121,25 @@ function repair_schedule(schedule_data)
 
         if no_improved_iters < INC_TABU_SIZE_ITER
             global max_tabu_size = INITIAL_MAX_TABU_SIZE
+            length(tabu_list) > INITIAL_MAX_TABU_SIZE &&
+                println("Reseting max tabu size to: $(max_tabu_size)")
         elseif length(tabu_list) == max_tabu_size
             global max_tabu_size += 1
+            println("Incrementing max tabu size to: $(max_tabu_size)")
         end
 
         if best_res.score == 0
+            println("We will not be better, finishing.")
             break
         end
+        println("Iteration best score: $(best_iter_res.score)")
     end
 
-    best_res.shifts
+    with_logger(logger) do
+        improved_penalty, errors =
+            score((workers, best_res.shifts), month_info, workers_info, true)
+        println("Penaly improved: '$(penalty)' -> '$(improved_penalty)'")
+    end
+
+    return best_res.shifts
 end
